d9216e91d3fe1ff0ad6a3040cd87dae181718ba8
reecebrend <reece.brend@wmfs.net>
2019-01-07 08:59:40 +0000

    test(): bulk up pretend-tymly for better testing

---
+++ test/fixtures/pretend-tymly/plugins/simpsons-plugin/lib/components/services/krusty-burger/index.js
@@ -0,0 +1,3 @@
+module.exports = () => {
+  console.log('Welcome to Krusty Burger, may I take your order?')
+}
--- test/fixtures/pretend-tymly/plugins/simpsons-plugin/lib/components/services/kwik-e-mart/doc/index.js
+++ test/fixtures/pretend-tymly/plugins/simpsons-plugin/lib/components/services/kwik-e-mart/doc/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   description: 'kwik-e-mart service',
-  quote: 'Thank you, come again'
+  quote: 'Thank you, come again',
+  example: require('./example')
 }
